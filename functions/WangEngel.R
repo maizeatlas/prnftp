bf860085562951ec40de8952bd7e3718f8492711
Maize ATLAS <maizeatlas@gmail.com>
2025-04-08 10:53:05 +0200

    clean script WangEngel.R

--- functions/WangEngel.R
+++ functions/WangEngel.R
@@ -1,9 +1,6 @@
 ##### Calculate the thermal time using formula (Wang & Engel 2017)
 
-
-# Helper function ----
-
-#slightly speeds up computation of functions of 1 argument when there are many more values in x than distinct values
+# Slightly speeds up computation of functions of 1 argument when there are many more values in x than distinct values
 apply_unique<- \(x, f) {
   ux <- unique(x)
   fux <- f(ux)
@@ -76,72 +73,6 @@ dTTsum <- function(dTmin, dTmax, julianDate, latitudeDeg, ...) {
   hTemp <- HourlyAirTemperature(dTmin, dTmax, julianDate, latitudeDeg, ...)
   n <- length(dTmin)
   .rowMeans(WangEngel(hTemp, n, ...), n, 24)
-}
-
-
-
-##### Calculate the thermal time using TT20 formula (Parent & Tardieu 2012)
-
-### Constantes 
-
-Ha <- 76800   
-Hd <- 285000
-Sd <- 933
-R <- 8.314
-
-### Temperature in Kelvin unit
-meteo_data_hourly$TK <- meteo_data_hourly$T2M + 273
-
-### Fonction theorique a 20 degresC 
-f20 <- (293*exp(-Ha/(R*293)))/(1+exp((Sd/R)-(Hd/(R*293))))
-
-### Function of temperature at X°C 
-meteo_data_hourly$Ft <- (meteo_data_hourly$TK*exp(-Ha/(R*meteo_data_hourly$TK))/(1+exp((Sd/R)-(Hd/(R*meteo_data_hourly$TK)))))
-
-
-### Get the corresponding time spent at 20°C in hour 
-#(number of hour(s) at 20°C corresponding to 1 hour at X°C)
-meteo_data_hourly <- meteo_data_hourly %>% 
-  dplyr::group_by(LAT, YEAR, MO, DY) %>%
-  dplyr::rowwise() %>%
-  dplyr::mutate(tt20 = Ft/f20) %>%
-  as.data.frame 
-
-### Cumsum of the hours a 20°C (tt20) for each day and dividing per 24 to get the number of day at 20°C for each day
-meteo_data_hourly  <- meteo_data_hourly %>% 
-  dplyr::group_by(LAT, YEAR, MO, DY) %>%
-  dplyr::mutate(sumtt20 = cumsum(tt20)) %>% #Get the cumulative hours at 20°C per day, (ultimately this will serve to get the number of day at 20°C)
-  dplyr::ungroup() %>%
-  as.data.frame()
-
-### Get the total number of day at 20°C per calendar day    
-summary_sumtt20 <- meteo_data_hourly %>% 
-dplyr::group_by(LAT, YEAR, MO, DY)%>%
-dplyr::arrange(YEAR, MO, DY, HR)%>%
-dplyr::slice(n())  ## extract the total cumulated temperature at 20°C per day
-
-
-##### Calculate the thermal time using GDD formula (Bonhomme et al. 1994)
-
-GrowingDegDays<- function(maxT, minT, optT, baseT){
-  if(maxT<=optT & minT>=baseT){
-    return(((maxT+minT)/2)-baseT)
-  } 
-  else if(minT<baseT & (maxT<=optT & maxT>=baseT)){
-    return(((maxT+baseT)/2)-baseT)
-  } 
-  else if(maxT>optT & minT>= baseT & optT>= minT){
-    return(((optT - (maxT - optT))+minT)/2 - baseT)
-  } 
-  else if(maxT>optT & minT <baseT){
-    return(((optT - (maxT - optT))+baseT)/2 - baseT)
-  }
-  else if(maxT<baseT & minT<baseT){
-    return(0)
-  }
-  else if(maxT>optT & minT>optT){
-    return(0)
-  }
 }
 
 
